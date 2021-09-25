1bb138ad4496eded2a71f2b9db07e13a785c6de4
leonardomrDev <leonardomr@protonmail.com>
2021-09-24 23:00:37 -0300

    full app/crud

--- db.py
+++ db.py
@@ -1,4 +1,6 @@
 import sqlite3
+from sqlite3 import *
+from sqlite3 import Cursor
 
 class Database:
     def __init__(self, db):
@@ -13,7 +15,7 @@ class Database:
 
     def add(self, nomeProduto, nomeMarca, valorProduto):
         self.cursor.execute("INSERT INTO produtos VALUES (NULL, ?, ?, ?)", (nomeProduto, nomeMarca, valorProduto))
-        self.cursor.commit()
+        self.connect.commit()
 
     def edit(self, id, nomeProduto, nomeMarca, valorProduto):
         self.cursor.execute("UPDATE produtos SET nomeProduto = ?, nomeMarca = ?, valorProduto = ? WHERE id = ?",
--- inventory_manager.py
+++ inventory_manager.py
@@ -1,24 +1,55 @@
 from tkinter import *
+from tkinter import messagebox
+from db import Database
 
-# Hook the Tk.Buttons to this function commands
-def migrate_list():
-    print('Migrate')
-#------------------------
+db = Database('App.db')
+
+# CRUD
+
+# Hook the Tk.Buttons to this function commands ---------------------------------
+def migrate():
+    item_list.delete(0, END)
+    for row in db.get():
+        item_list.insert(END, row)
+#---------------------------------------------------------------------------------
+def select(event):
+    global selectedItem
+    index = item_list.curselection()[0]
+    selectedItem = item_list.get(index)
+    print(selectedItem)
+
+    itemname_entry.delete(0, END)
+    itemname_entry.insert(END, selectedItem[1])
+    
+    itembrand_entry.delete(0, END)
+    itembrand_entry.insert(END, selectedItem[2])
+
+    itemprice_entry.delete(0, END)
+    itemprice_entry.insert(END, selectedItem[3])
+#------------------------------------------------------------------------------------
 def add():
-    print('Add Item')
-#------------------------
+    if item_text.get() == "" or brand_text.get() == "" or itemprice_text.get() == "":
+        messagebox.showerror('Required Fields', "Please, fill all the fields! ")
+        return
+    db.add(item_text.get(), brand_text.get(), itemprice_text.get())
+    item_list.delete(0, END)
+    item_list.insert(END, (item_text.get(), brand_text.get(), itemprice_text.get()))
+    migrate()
+#-------------------------------------------------------------------------------------
 def remove():
-    print('Remove Item')
-#------------------------
+    db.remove(selectedItem[0])
+    migrate()
+#------------------------------------------------------------------------------------
 def edit():
-    print('Edit Item')
-# App Window -----------------------------------------------
+    db.edit(selectedItem[0], item_text.get(), brand_text.get(), itemprice_text.get())
+    migrate()
+# App Window ------------------------------------------------------------------------
 app = Tk()
 app.title("PCStock: Inventory Manager for Computer Parts")
 app.geometry("800x400")
-#-----------------------------------------------------------
+#-------------------------------------------------------------------------------------
 
-# Item Insert Labels ----------------------------------------------------
+# Item Insert Labels -----------------------------------------------------------------
 item_text = StringVar()
 itemname_label = Label(app, text='Item Name', font=("bold", 12), pady= 10, padx=20)
 itemname_label.grid(row= 0, column= 0, sticky=W)
@@ -31,36 +62,30 @@ itembrand_label.grid(row= 1, column= 0, sticky=W)
 itembrand_entry = Entry(app, textvariable=brand_text)
 itembrand_entry.grid(row= 1, column= 1, sticky=W)
 
-price_text = StringVar()
-price_label = Label(app, text='Item Price', font=("bold", 12), pady= 10, padx=20)
-price_label.grid(row= 2, column= 0, sticky=W)
-price_entry = Entry(app, textvariable=price_text)
-price_entry.grid(row= 2, column= 1, sticky=W)
-#----------------------------------------------------------------------------
+itemprice_text = StringVar()
+itemprice_label = Label(app, text='Item Price', font=("bold", 12), pady= 10, padx=20)
+itemprice_label.grid(row= 2, column= 0, sticky=W)
+itemprice_entry = Entry(app, textvariable=itemprice_text)
+itemprice_entry.grid(row= 2, column= 1, sticky=W)
+#---------------------------------------------------------------------------------------
 
-# List of Items on Inventory ------------------------------------------------
+# List of Items on Inventory ----------------------------------------------------
 item_list = Listbox(app, height= 10, width= 60, border= 0)
 item_list.grid(row= 3, column= 0, columnspan= 3, rowspan= 6, pady= 20, padx= 20)
-#----------------------------------------------------------------------------
+item_list.bind('<<ListboxSelect>>', select)
+#---------------------------------------------------------------------------------
 
-# Scrollbar for the List of Items in Inventory-------------------------------
+# Scrollbar for the List of Items in Inventory----
 scrollbar = Scrollbar(app).grid(row=3, column=3)
-# Connects the Scrollbar to the list
-"""
-item_list.configure(yscrollcommand= scrollbar.set)
-scrollbar.configure(command=item_list.yview)
-#-----------------------------------------------------------------------------
-"""
-
-#------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------
 Button(app, text="Add", width= 12, command=add, font=("bold", 12)).grid(row=10, column=0, sticky=W, padx=20)
 Button(app, text="Edit", width= 12, command=edit, font=("bold", 12)).grid(row=10, column=1, sticky=W, padx=20)
 Button(app, text="Delete", width= 12, command=remove, font=("bold", 12)).grid(row=10, column=2, sticky=W, padx=20)
-#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------
 
-# Migrate our Data
-migrate_list()
-# Command to Run the App -------------------
+migrate()
+
+# Command to Run the App --------------------
 app.mainloop()
 #--------------------------------------------
 
